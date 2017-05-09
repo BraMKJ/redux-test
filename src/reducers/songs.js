54c6d2d26bd5691b82a5759dbda1c69e0346efc0
Bram Dijkhuis | Kramp <bram.dijkhuis@kramp.com>
2017-05-09 14:10:54 +0200

    Removed lodash clone and extend from songs reducer

--- src/reducers/songs.js
+++ src/reducers/songs.js
@@ -1,17 +1,16 @@
-import extend from 'lodash/extend';
-import clone from 'lodash/clone';
 import initialState from '../initialState'
 
 export default function songsReducer(state = initialState.songs, action) {
   switch(action.type) {
     case 'ADD_SONG':
-      return extend(clone(state), {
+      return {
+        ...state,
         [action.key]: {
           name: action.name,
           artist: action.artist,
           status: action.status
         }
-      })
+      }
     default:
       return state
   }
